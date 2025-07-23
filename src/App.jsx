36ea2b3f9b83f4a2ec0894ff39b17c0819a892c1
bharratgehlot <161546117+bharratgehlot@users.noreply.github.com>
2025-07-23 18:50:38 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+// src/App.jsx
+function App() {
+  return (
+    <>
+    <div className="container">
+      <div>
+        <h1>QuizMaster</h1>
+        <p>Guess the right country by its flag!</p>
+        <button type="submit">
+          PLAY
+        </button>
+      </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
+
